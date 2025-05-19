5798b4d143013517cad30c1a00173914be54a884
metallica81 <metallica923@gamil.com>
2025-05-19 12:03:58 +0300

    feat: continue dev auth (add token)

--- src/actions/postAuthData.ts
+++ src/actions/postAuthData.ts
@@ -11,27 +11,30 @@ interface AuthDataTypes {
 
 export async function postAuthData({ login, mail, password, csrf_token }: AuthDataTypes) {
   try {
+    console.log("Отправляемый CSRF-токен:", csrf_token);
     const response = await axios.post(
       "https://test-music-app.ru/music_web-app_backend/API/reg.php",
       {
         login,
         mail,
-        password,
-        csrf_token
+        password
       },
       {
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrf_token
         },
       }
     );
 
     const data = response.data;
+    console.log("Ответ сервера:", data);
     if (!data.success) {
       throw new Error(data.error || "Ошибка регистрации");
     }
     return data; 
   } catch (error) {
+    console.error("Ошибка в postAuthData:", error);
     throw new Error(
       error instanceof Error ? error.message : "Ошибка подключения к серверу"
     );
