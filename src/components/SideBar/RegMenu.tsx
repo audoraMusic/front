bff7e2c12a3cb34eea9c2cad084d08039af41cc2
metallica81 <metallica923@gamil.com>
2025-07-02 21:32:34 +0300

    fix: speed up routing

--- src/components/SideBar/RegMenu.tsx
+++ src/components/SideBar/RegMenu.tsx
@@ -1,12 +1,13 @@
 import { TabLink } from "../TabLink";
+import styles from "./SideBar.module.scss";
 
 export function RegMenu() {
     return (
         <>
-            <TabLink to="/auth" variant="outline-info">
+            <TabLink to="/auth" variant="outline-info" externalClassnames={styles.link}>
                 Войти
             </TabLink>
-            <TabLink to="/registration" variant="outline-info">
+            <TabLink to="/registration" variant="outline-info" externalClassnames={styles.link}>
                 Зарегистрироваться
             </TabLink>
         </>
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,24 +6,30 @@ import { useSelector } from "react-redux";
 import { Login } from "./Login";
 import { RegMenu } from "./RegMenu";
 
-export function SideBar({ children }: {children?: ReactNode}) {
+export function SideBar({ children }: { children?: ReactNode }) {
     const login = useSelector((state: RootState) => state.auth.login);
     const isAuth = useSelector((state: RootState) => state.auth.value);
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.sideBar}>
-                <TabLink to="/main">Главная</TabLink>
-                <TabLink to="/favorities">Мои треки</TabLink>
-                <TabLink to="/playlists">Плейлисты</TabLink>
-                <TabLink to="/trendes">Тренды</TabLink>
-                <TabLink to="/history">История</TabLink>
+                <TabLink to="/main" externalClassnames={styles.link}>
+                    Главная
+                </TabLink>
+                <TabLink to="/favorities" externalClassnames={styles.link}>
+                    Мои треки
+                </TabLink>
+                <TabLink to="/playlists" externalClassnames={styles.link}>
+                    Плейлисты
+                </TabLink>
+                <TabLink to="/trendes" externalClassnames={styles.link}>
+                    Тренды
+                </TabLink>
+                <TabLink to="/history" externalClassnames={styles.link}>
+                    История
+                </TabLink>
                 <div className="mt-auto d-flex flex-column gap-2">
-                    {!isAuth ? (
-                        <RegMenu />
-                    ) : (
-                        <Login login={login} />
-                    )}
+                    {!isAuth ? <RegMenu /> : <Login login={login} />}
                 </div>
             </div>
             {children}
