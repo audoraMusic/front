6842406edfcf7173d756ca888f7d295fbbfc841d
metallica81 <metallica923@gamil.com>
2025-07-10 21:12:51 +0300

    feat: change panelControl buttons in player

---
+++ src/pageElements/main/Player/ControlPanel.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import StopIcon from "../../../public/stop.svg";
+import LeftArrow from "../../../public/lArrow.svg";
+import RightArrow from "../../../public/rArrow.svg";
+import { Button } from "react-bootstrap";
+import styles from "./Player.module.scss"
+
+export function ControlPanel() {
+    return (
+        <div className={styles.panel}>
+            <Button variant="secondary" className="mx-1 py-1 px-2 rounded">
+                <Image src={LeftArrow} alt="stop" width={30} height={30} />
+            </Button>
+            <Button variant="secondary" className="mx-1 py-1 px-2 rounded">
+                <Image src={StopIcon} alt="stop" width={30} height={30} />
+            </Button>
+            <Button variant="secondary" className="mx-1 py-1 px-2 rounded">
+                <Image src={RightArrow} alt="stop" width={30} height={30} />
+            </Button>
+        </div>
+    );
+}
--- src/redux/slices/playerSlice.ts
+++ src/redux/slices/playerSlice.ts
@@ -1,24 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { BoolState } from './sliceInterfaces';
+import { createSlice } from "@reduxjs/toolkit";
+import { BoolState } from "./sliceInterfaces";
 
 const initialState: BoolState = {
-  value: false,
-}
+    value: false,
+};
 
 export const playerSlice = createSlice({
-  name: 'player',
-  initialState,
-  reducers: {
-    playOn: (state) => {
-        state.value = true
+    name: "player",
+    initialState,
+    reducers: {
+        playOn: (state) => {
+            state.value = true;
+        },
+        playOff: (state) => {
+            state.value = false;
+        },
     },
-    playOff: (state) => {
-        state.value = false
-    }
-  },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { playOn, playOff } = playerSlice.actions
+export const { playOn, playOff } = playerSlice.actions;
 
-export default playerSlice.reducer
+export default playerSlice.reducer;
