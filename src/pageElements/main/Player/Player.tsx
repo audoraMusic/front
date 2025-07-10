1ed44288f094530bcee2ad69545d5a3f17348bac
metallica81 <metallica923@gamil.com>
2025-07-10 21:41:15 +0300

    feat: beggin dev player

--- src/pageElements/main/Player/Player.tsx
+++ src/pageElements/main/Player/Player.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "react-bootstrap";
 import { ControlPanel } from "./ControlPanel"
 import styles from "../../../app/main/Main.module.scss";
+import { ProgressBar } from "./ProgressBar";
 
 export function Player() {
     return (
@@ -9,7 +10,7 @@ export function Player() {
             bg="secondary"
             className="d-flex justify-content-between align-items-center px-3 py-2 rounded-top"
         >
-            
+            <ProgressBar />
             <ControlPanel />
         </Navbar>
     );
---
+++ src/pageElements/main/Player/ProgressBar.tsx
@@ -0,0 +1,5 @@
+import styles from "./Player.module.scss";
+
+export function ProgressBar() {
+    return <div className={styles.line}></div>;
+}
