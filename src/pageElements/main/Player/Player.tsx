c74529968d86e26a6f4f5822a51c367508009b1f
metallica81 <metallica923@gamil.com>
2025-07-13 13:32:56 +0300

    feat: dev bar

--- src/pageElements/main/Player/Player.tsx
+++ src/pageElements/main/Player/Player.tsx
@@ -1,18 +1,31 @@
 import { Navbar } from "react-bootstrap";
-import { ControlPanel } from "./ControlPanel"
+import { ControlPanel } from "./ControlPanel";
 import styles from "./Player.module.scss";
 import { ProgressBar } from "./ProgressBar";
+import { useState } from "react";
 
 export function Player() {
+    const duration = 240;
+    const [currentTime, setCurrentTime] = useState(0);
+
+    const handleSeek = (time: number) => {
+        console.log("Перематываем на:", time);
+        setCurrentTime(time);
+    };
+
     return (
         <Navbar
             fixed="bottom"
             bg="secondary"
             className={`${styles.player} d-flex justify-content-between align-items-center rounded`}
         >
-            {/* test rep */}
-            <ProgressBar />
-            <ControlPanel />
+            <ProgressBar
+                duration={duration}
+                currentTime={currentTime}
+                onSeek={handleSeek}
+            >
+                <ControlPanel />
+            </ProgressBar>
         </Navbar>
     );
 }
--- src/pageElements/main/Player/ProgressBar.tsx
+++ src/pageElements/main/Player/ProgressBar.tsx
@@ -1,5 +1,44 @@
 import styles from "./Player.module.scss";
+import { ReactNode } from "react";
+import { useRef } from "react";
 
-export function ProgressBar() {
-    return <div className={styles.line}></div>;
+interface ProgressBarProps {
+    children: ReactNode;
+    duration: number;
+    currentTime: number;
+    onSeek: (time: number) => void;
+}
+
+export function ProgressBar({
+    children,
+    duration,
+    currentTime,
+    onSeek,
+}: ProgressBarProps) {
+    const barRef = useRef<HTMLDivElement>(null);
+
+    const progress = duration ? (currentTime / duration) * 100 : 0;
+
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!barRef.current) return;
+
+        const rect = barRef.current.getBoundingClientRect();
+        const clickX = e.clientX - rect.left;
+        const width = rect.width;
+
+        const percent = clickX / width;
+        const newTime = duration * percent;
+
+        onSeek(newTime);
+    };
+
+    return (
+        <div ref={barRef} className={styles.bar} onClick={handleClick}>
+            <div
+                className={styles.progressLine}
+                style={{ width: `${progress}%` }}
+            />
+            <div className={styles.panelInProgress}>{children}</div>
+        </div>
+    );
 }
